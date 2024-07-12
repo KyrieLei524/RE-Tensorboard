f3a2933254f16662532b1243e7e4cc22229d2bd9
JingGai0524 <1530730178@qq.com>
2024-07-12 11:29:07 +0800

    add select event

--- src/components/dagre_try.vue
+++ src/components/dagre_try.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="node-info-panel" v-if="selectedNode">
     <h3>{{ selectedNode.raw_info['name'] }}</h3>
     <p><strong>Operation:</strong> {{ selectedNode.raw_info['op'] }}</p>
@@ -8,7 +7,7 @@
       <div v-for="(value, key) in selectedNode.raw_info['attr']" :key="key" class="attribute">
         <p><strong>{{ key }}:</strong> {{ value }}</p>
       </div>
-       </div>
+    </div>
     <div class="inputs-section" v-if="selectedNode">
       <h4>Inputs ({{ selectedNode.input_num }})</h4>
       <div v-for="input in selectedNode.raw_info['input_edges']" :key="input" class="input">
@@ -22,7 +21,6 @@
       </div>
     </div>
   </div>
-
   <div>
     <svg ref="svg" :width="width" :height="height"></svg>
   </div>
@@ -30,12 +28,12 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, computed, watch } from 'vue';
 import * as d3 from 'd3';
 import dagre from 'dagre';
 import node_data from "../../../../nodes.json";
 import { componentToSlot } from 'element-plus/es/components/table-v2/src/utils.mjs';
-const width = 800;
+const width = 1200;
 const height = 800;
 const svg = ref(null);
 
@@ -73,7 +71,6 @@ my_nodes.set('root', root);
 
 
 node_data.forEach(element => {
-  // console.log(element.attr);
   var node = {
     label: element.name,
     children: [],
@@ -89,7 +86,7 @@ node_data.forEach(element => {
     input_num: element.input_edges.length,
     output_num: element.output_edges.length
   };
-  console.log(element.input_edges.length);
+  // console.log(element.input_edges.length);
   if (node.parent === null || element.layer_num === 1) {
     node.parent = 'root';
   }
@@ -146,8 +143,6 @@ node_data.forEach(element => {
   })
 });
 
-console.log(my_nodes.get('ExpandDims'));
-console.log(my_nodes.get('ExpandDims/dim'));
 //现在已经处理了所有的边的信息，接下来要把边的信息添加到node里面去
 my_edges.forEach((value, key) => {
   const source = value.source;
@@ -163,7 +158,6 @@ my_edges.forEach((value, key) => {
   if (my_nodes.get(source).parent === my_nodes.get(target).parent) {
     //如果父节点相同 说明他们存在于同一个子图当中 那么就把这条边添加到子图的边里面去
     const parent_name = my_nodes.get(source).parent;
-    // console.log(my_nodes.get(parent_name));
     my_nodes.get(parent_name).edges.push(edge_obj);
   }
 });
@@ -172,7 +166,7 @@ my_edges.forEach((value, key) => {
 
 var map = my_nodes;
 
-console.log(root);
+
 
 var selectedNode = ref(root);
 
@@ -263,8 +257,9 @@ function draw_root(root, svgGroup) {
   // Draw nodes
   g.nodes().forEach(node => {
     const n = g.node(node);
-    // console.log(node)
+    // console.log(n)
     const nodeGroup = svgGroup.append("g")
+      .attr("id", node.replace(/\//g, "-")) //建立id,方便后续再次寻找，注意这里名字不能带反斜杠，所以需要替换
       .attr("transform", `translate(${n.x - n.width / 2 + root.x}, ${n.y - n.height / 2 + root.y})`);
 
     // 画完图以后就应该更新刚刚画的这个节点的位置信息 是绝对位置 因为每次画的时候已经是绝对位置了
@@ -288,9 +283,12 @@ function draw_root(root, svgGroup) {
         draw_root(root, svgGroup);
       })
       .on("click", function (event) {
-        console.log("click");
+        if(selectedNode.value.label === node_obj.label){
+          console.log("same node");
+          selectedNode.value = root;
+          return;
+        }
         selectedNode.value = node_obj;
-        console.log(selectedNode);
       });
 
     nodeGroup.append("text")
@@ -321,97 +319,40 @@ function draw_root(root, svgGroup) {
       draw_root(n, svgGroup);
     }
   }
-
 }
 
+watch(selectedNode, (newVal, oldVal) => {
+
+  if(oldVal === newVal){
+    selectedNode.value = null;
+    return;
+  }
+  //更新新选中节点的样式
+  if (newVal.raw_info['name']) {
+    const id = newVal.raw_info['name'].toString().replace(/\//g, "-");
+    console.log(id);
+
+    const node = d3.select(`#${id}`);
+    node.select("rect").attr("stroke", "#ff0000").attr("fill", "#f5f5f5");
+  }
+
+  //更新旧选中节点的样式
+  if (oldVal.raw_info['name']) {
+    const old_id = oldVal.raw_info['name'].toString().replace(/\//g, "-");
+    const old_node = d3.select(`#${old_id}`);
+    old_node.select("rect").attr("stroke", "#333").attr("fill", "#fff");
+  }
+
+});
 
 
-const renderGraph = () => {
-  // Create a new directed graph
-
-  // Set an object for the graph label
-  g.setGraph({});
-
-  // Default to assigning a new object as a label for each new edge.
-  g.setDefaultEdgeLabel(() => ({}));
-
-  // 方向
-  g.graph().rankdir = "BT";
-
-  // Add nodes to the graph
-  g.setNode("A", { label: "A", width: nodeWidth, height: nodeHeight });
-  g.setNode("B", { label: "B", width: nodeWidth, height: nodeHeight });
-  g.setNode("C", { label: "C", width: nodeWidth, height: nodeHeight });
-
-  // Add edges to the graph
-  g.setEdge("A", "B");
-  g.setEdge("B", "C");
-  g.setEdge("C", "A");
-
-  // Layout the graph
-  dagre.layout(g);
-
-  // Create an SVG group
-  const svgEl = d3.select(svg.value);
-
-  // Create a container for zoom and pan
-  const zoomGroup = svgEl.append("g");
-
-  // Create a group for the graph elements
-  const svgGroup = zoomGroup.append("g");
-
-  // Draw edges
-  g.edges().forEach(edge => {
-    const points = g.edge(edge).points;
-    const lineGenerator = d3.line()
-      .x(d => d.x)
-      .y(d => d.y);
-
-    svgGroup.append("path")
-      .attr("d", lineGenerator(points))
-      .attr("class", "edgePath")
-      .attr("stroke", "#333")
-      .attr("stroke-width", 1.5)
-      .attr("fill", "none");
-  });
-
-  // Draw nodes
-  g.nodes().forEach(node => {
-    const n = g.node(node);
-    const nodeGroup = svgGroup.append("g")
-      .attr("transform", `translate(${n.x - n.width / 2}, ${n.y - n.height / 2})`);
-
-    nodeGroup.append("rect")
-      .attr("width", n.width - 40)
-      .attr("height", n.height - 60)
-      .attr("stroke", "#333")
-      .attr("fill", "#fff");
-
-    nodeGroup.append("text")
-      .attr("x", n.width / 2)
-      .attr("y", n.height / 2)
-      .attr("dy", ".35em")
-      .attr("text-anchor", "middle")
-      .text(n.label);
-  });
-
-  // // Center the graph
-  // const xCenterOffset = (svgEl.attr("width") - g.graph().width) / 2;
-  // svgGroup.attr("transform", `translate(${xCenterOffset}, 20)`);
-  // svgEl.attr("height", g.graph().height + 40);
-
-};
 onMounted(() => {
-  // renderGraph();
 
   const svgEl = d3.select(svg.value);
   const zoomGroup = svgEl.append("g");
   const svgGroup = zoomGroup.append("g");
   //所有内容都画在svgGroup上
-
-
   compute_node(root);
-  // console.log(root);
   draw_root(root, svgGroup);
 
   const zoom = d3.zoom().on("zoom", function (event) {
@@ -440,8 +381,6 @@ onMounted(() => {
   padding: 10px;
   border: 1px solid #ccc;
   background-color: white;
-
-
 }
 
 .attributes-section,
